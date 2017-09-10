8db5c0164136d24cbd9c5965565d9a7824475ee3
eevee <quine.618033988@gmail.com>
2017-09-10 01:29:00 -0400

    Fix some grammar

--- foxybot/config.py
+++ foxybot/config.py
@@ -5,11 +5,12 @@ available to any files that import this.
 
 import json
 
+
 config = {}
 
 try:
     with open('config.json', 'r') as f:
         config = json.load(f)
 except OSError:
-    print("Error reading config.json make sure the file exists and is readable")
+    print("Error reading config.json. Make sure the file exists and is readable")
     exit()
