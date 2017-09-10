6cfcc9ad5a50ed6eacf9eae84c9dd6e3c10d42b2
eevee <quine.618033988@gmail.com>
2017-09-10 13:52:01 -0400

    created registrar.py to manage a table of loaded commands

---
+++ foxybot/registrar.py
@@ -0,0 +1,34 @@
+
+
+class CommandRegistrar():
+    """A singleton to manage the command table and command execution"""
+
+    _instance = None
+
+    def __init__(self):
+        self.command_table = {}
+
+    @staticmethod
+    def instance():
+        """Get the singleton, create an instance if needed"""
+        if not CommandRegistrar._instance:
+            CommandRegistrar._instance = CommandRegistrar()
+        return CommandRegistrar._instance
+
+    @staticmethod
+    async def execute_command(shards, shard, msg):
+        # !roll 100 -> 'roll'
+        instance = CommandRegistrar.instance()
+        command = msg.content[1:].split(' ')[0].lower()
+
+        if command in instance.command_table.keys():
+            await instance.command_table[command].execute(shards, shard, msg)
+
+
+    @property
+    def loaded_commands(self):
+        return [command.name for command in set(self._command_table.values())]
+
+    @property
+    def loaded_aliases(self):
+        return list(command_table.keys())
