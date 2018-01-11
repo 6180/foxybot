4678b9e171ce3542e93bb123b3148750c2e6baeb
6180 <quine.618033988@gmail.com>
2018-01-11 00:48:26 -0500

    Switch to aiohttp instead of requests for GET requests in xkcd command

--- foxybot/commands/xkcd.py
+++ foxybot/commands/xkcd.py
@@ -1,5 +1,6 @@
 """Command to retrieve random xkcd comic"""
 
+import aiohttp
 import requests
 
 from discord import Embed
@@ -12,8 +13,12 @@ class Xkcd(AbstractCommand):
 
     async def execute(self, shards, client, msg):
         # TODO: allow specifying a numbered comic or search term to retrieve
-        url = requests.get('http://c.xkcd.com/random/comic/').url
-        comic = requests.get(f'{url}/info.0.json').json()
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get('http://c.xkcd.com/random/comic/') as resp:
+                url = resp.url
+            async with session.get(f'{url}/info.0.json') as resp:
+                comic = await resp.json()
 
         embed = Embed()
         embed.colour = 0x6699FF
